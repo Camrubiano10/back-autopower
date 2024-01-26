7f588af1b03aa80a526045ce5fd2b1095fb4db6c
Ivan Leon <leonsanp@gmail.com>
2024-01-26 09:24:33 -0500

    Back 100%

--- client/views.py
+++ client/views.py
@@ -15,3 +15,18 @@ class ClientViewSet(ModelViewSet):
         return Response(serializer.data)
 
 
+from rest_framework.viewsets import ModelViewSet
+from .models import Client
+from .serializers import ClientSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class ClientViewSet(ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        search=Client.objects.get(license_plate=kwargs['pk'])
+        serializer = self.serializer_class(search)
+        return Response(serializer.data)
